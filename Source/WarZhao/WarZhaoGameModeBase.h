9e1121e67ce67eb926cfd5b7907c0f553664fb4f
[o0ojenny] <[o0o_jenny@naver.com]>
2023-07-11 12:51:54 +0900

    GO

---
+++ Source/WarZhao/WarZhaoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarZhaoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARZHAO_API AWarZhaoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
