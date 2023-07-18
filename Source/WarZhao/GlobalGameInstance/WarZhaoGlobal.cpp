9f4f8b7170850468231204d2e06a3b96d2e3de62
[o0ojenny] <[o0o_jenny@naver.com]>
2023-07-18 12:51:02 +0900

    try to Debug

---
+++ Source/WarZhao/GlobalGameInstance/WarZhaoGlobal.cpp
@@ -0,0 +1,8 @@
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//
+//#include "GlobalGameInstance/WarZhaoGlobal.h"
+//
+//
+////static void WarDebugTextPring(clas AActor* _Object, const FString& _Text)
+//
