87c18febf581e278f3ebc54e834d774a5e240630
[o0ojenny] <[o0o_jenny@naver.com]>
2023-07-20 12:52:43 +0900

    got it

---
+++ Source/WarZhao/WarCharacter/EnumsAll.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class WarAniState : uint8
+{
+	None UMETA(DisplayName = "사용안함"),
+	Idle UMETA(DisplayName = "서있기"),
+	LeftMove UMETA(DisplayName = "왼쪽이동"),
+	RightMove UMETA(DisplayName = "오른쪽이동"),
+	ForwardMove UMETA(DisplayName = "앞으로이동"),
+	BackwardMove UMETA(DisplayName = "뒤로이동"),
+	Jump UMETA(DisplayName = "점프"),
+	Attack UMETA(DisplayName = "공격"),
+};
--- Source/WarZhao/WarCharacter/WarCharacter.h
+++ Source/WarZhao/WarCharacter/WarCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnumsAll.h"
 #include "WarCharacter.generated.h"
 
 UCLASS()
@@ -15,15 +16,42 @@ public:
 	// Sets default values for this character's properties
 	AWarCharacter();
 
+	UFUNCTION(BlueprintCallable)
+	void AnimationTick();
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+public:
+	void AttackAction();
+	void JumpAction();
+	void JumpAxis(float Rate);
+
+	void MoveRight(float Val);
+	void MoveForward(float Val);
+	void TurnAtRate(float Rate);
+	void LookUpAtRate(float Rate);
+
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = "Pawn")
+		float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn")
+		float BaseLookUpRate;
+
+	bool AxisJump = false;
+
+	UPROPERTY(Category = "GameModeValue", EditAnywhere, BlueprintReadWrite)
+		WarAniState AniState = WarAniState::Idle;
+
+	UPROPERTY(Category = "AnimationValue", EditAnywhere, BlueprintReadWrite)
+		TMap<WarAniState, class UAnimMontage*> AllAnimations;
+
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+		UStaticMeshComponent* WeaponMesh;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+		TArray<UStaticMesh*> WeaponArrays;
 };
