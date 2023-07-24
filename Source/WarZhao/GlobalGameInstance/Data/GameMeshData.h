58b9227c78740524b4313db96738079f39b7b560
{y2jnwo2} <{y2jnwo2@gmail.com}>
2023-07-25 01:00:44 +0900

    Anim Retarget

---
+++ Source/WarZhao/GlobalGameInstance/Data/GameMeshData.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "UObject/NoExportTypes.h"
+#include "GameMeshData.generated.h"
+
+USTRUCT(BlueprintType)
+struct WARZHAO_API FGameMeshData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	FGameMeshData() {}
+	~FGameMeshData() {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	class UStaticMesh* Mesh;
+
+};
