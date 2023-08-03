0df25a9a4fcd05152374041ad51bdc7ee7b11390
[o0ojenny] <[o0o_jenny@naver.com]>
2023-08-03 12:47:07 +0900

    AI ready

---
+++ Source/WarZhao/AI/AIEnum.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+// 애니메이션에 집중할거냐
+// 캐릭터에 집중할 거냐?
+
+// 몬스터와 플레이어가 정말 모든 애니메이션 상태에 대해서 공유가 가능하냐를 생각해보고
+// 불가능하다면 Enum은 사용이 안된다.
+
+
+UENUM(BlueprintType)
+enum class AIAniState : uint8
+{
+	None UMETA(DisplayName = "사용안함"),
+	Idle UMETA(DisplayName = "서있기"),
+	LeftMove UMETA(DisplayName = "왼쪽이동"),
+	RightMove UMETA(DisplayName = "오른쪽이동"),
+	ForwardMove UMETA(DisplayName = "앞으로이동"),
+	BackwardMove UMETA(DisplayName = "뒤로이동"),
+	Jump UMETA(DisplayName = "점프"),
+	Attack UMETA(DisplayName = "공격"),
+};
+
--- Source/WarZhao/GlobalGameInstance/AICharacter.h
+++ Source/WarZhao/GlobalGameInstance/AICharacter.h
@@ -1,17 +1,58 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GlobalGameInstance/GlobalCharacter.h"
-#include "AICharacter.generated.h"
-
-/**
- * 
- */
-UCLASS()
-class WARZHAO_API AAICharacter : public AGlobalCharacter
-{
-	GENERATED_BODY()
-	
-};
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "GlobalGameInstance/GlobalCharacter.h"
+//#include "AI/AIEnum.h"
+//#include "AICharacter.generated.h"
+//
+///**
+// * 
+// */
+//UCLASS()
+//class WARZHAO_API AAICharacter : public AGlobalCharacter
+//{
+//	GENERATED_BODY()
+//	
+//
+//public:
+//	AAICharacter();
+//
+//	void AttackAction();
+//	void JumpAction();
+//	void MoveRight(float Val);
+//	void MOveForWard(float Val);
+//	void TurnAtRate(float Rate);
+//	void LookUpAtRate(float Rate);
+//
+//	void StatusWindowOnOff();
+//
+//protected:
+//	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+//
+//	void BeginPlay() override;
+//
+//private:
+//
+//	UPROPERTY(Category = "Component", EditAnyWhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+//		class USpringArmComponent* SpringArmComponent;
+//
+//	UPROPERTY(Category = "Component", EditAnyWhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+//		class UCameraComponent* CameraComponent;
+//
+//	void Tick(float _Delta) override;
+//
+//	UPROPERTY(Category = "Component", EditAnyWhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+//		TMap<AIAniState, class UAnimMontage*> MapAnimation;
+//
+//	UFUNCTION()
+//		void MontageEnd(UAnimMontage* Anim, bool _Inter);
+//
+//	UFUNCTION()
+//		void AnimNotifyBegin(FName NotifyName, const FBranchingPointNotifyPayload& BranchingPointPayload);
+//
+//	float Speed = 800.0f;
+//
+//	
+//};
